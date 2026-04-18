36564ec219ec23abb5cfa855618bdb65dfe59db8
Haval-Jalal <Haval.jalal@hotmail.com>
2026-04-18 13:57:34 +0200

    Skapat mappar i appliktion-lager och skapat DTOs

---
+++ CleanCarApi.Application/DTOs/UpdateCarDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanCarApi.Application.DTOs
+{
+    public class UpdateCarDto
+    {
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public int FuelType { get; set; }
+        public int BrandId { get; set; }
+    }
+}
